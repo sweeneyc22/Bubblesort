39e6a6a9413141d89bafb0cd570be1464da0aa04
Colby <70588351+sweeneyc22@users.noreply.github.com>
2022-07-08 16:28:50 -0700

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,28 @@
+//
+// Created by dxfan on 7/8/2022.
+//
+
+#include <vector>
+#include "functions.h"
+#include "iostream"
+#include <algorithm>
+
+
+
+
+void bubbleSort(std::vector<int> dataset, long long setsize)
+{
+
+}
+
+void printVector(std::vector<int> dataset)
+{
+    for (int i = 0; i < dataset.size() - 1; i++)
+    {
+        std::cout << dataset.at(i) << "\t ";
+    }
+    std::cout << "\n";
+}
+
+
+
