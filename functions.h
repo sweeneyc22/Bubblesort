b3bd5afc3bc68a1555386550da4617dce8b867f9
Colby <70588351+sweeneyc22@users.noreply.github.com>
2022-07-08 16:29:20 -0700

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,12 @@
+//
+// Created by Colby on 7/8/2022.
+//
+
+#ifndef BUBBLESORT_FUNCTIONS_H
+#define BUBBLESORT_FUNCTIONS_H
+
+
+void printVector(std::vector<int> dataset);
+void bubbleSort(std::vector<int> dataset, long long setsize);
+
+#endif //BUBBLESORT_FUNCTIONS_H
